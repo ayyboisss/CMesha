32e63fc386d25f0bb375edf794e480211d097cb0
ayyboisss <kentquilestino.lols@gmail.com>
2024-05-09 22:27:29 +1200

    HTML templates and SQLalchemy setup done!

--- server.py
+++ server.py
@@ -1,2 +1,9 @@
-import database
+from database import app, AnemometerDatum, Classroom, LoudnessDatum, TemperatureHumidityDatum, Teacher, t_TeacherClassrooms
+from flask import render_template
 
+@app.route("/")
+def home():
+    return(render_template("main.html"))
+
+if "__main__" == __name__:
+    app.run(debug=False) 
