b7e5eb5a87bf774d7f4ad01d4c1bd8e23b880d92
ayyboisss <kentquilestino.lols@gmail.com>
2024-05-22 13:22:55 +1200

    sqlalchemy paginate testing

--- server.py
+++ server.py
@@ -3,7 +3,9 @@ from flask import render_template
 
 @app.route("/")
 def home():
-    print(LoudnessData.query.with_entities(LoudnessData.LoudnessReading, LoudnessData.ClassroomID).all())
+    the_page = LoudnessData.query.paginate(per_page=6)
+    for i in the_page.items:
+        print(i.ClassroomID, i.LoudnessReading)
     return(render_template("main.html"))
 
 if "__main__" == __name__:
