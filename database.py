20cc1261fa32a43dea77603f0bbd38b0764f083f
BURNSIDE HIGH SCHOOL <code@burnside.school.nz>
2024-05-22 14:15:31 +1200

    More data, start making analytics page

--- database.py
+++ database.py
@@ -22,7 +22,6 @@ class AnemometerDatum(db.Model):
     ID = db.Column(db.Integer, primary_key=True)
     ClassroomID = db.Column(db.ForeignKey('Classrooms.ClassroomID'))
     AnemometerReading = db.Column(db.Float)
-    DateRecorded = db.Column(db.Date)
 
     Classroom = db.relationship('Classroom', primaryjoin='AnemometerDatum.ClassroomID == Classroom.ClassroomID', backref='anemometer_data')
 
@@ -43,7 +42,6 @@ class LoudnessData(db.Model):
     ID = db.Column(db.Integer, primary_key=True)
     ClassroomID = db.Column(db.ForeignKey('Classrooms.ClassroomID'))
     LoudnessReading = db.Column(db.Float)
-    DateRecorded = db.Column(db.Date)
 
     Classroom = db.relationship('Classroom', primaryjoin='LoudnessData.ClassroomID == Classroom.ClassroomID', backref='loudness_data')
 
@@ -73,6 +71,5 @@ class TemperatureHumidityDatum(db.Model):
     ClassroomID = db.Column(db.ForeignKey('Classrooms.ClassroomID'))
     TemperatureReading = db.Column(db.Float)
     HumidityReading = db.Column(db.Float)
-    DateRecorded = db.Column(db.Date)
 
     Classroom = db.relationship('Classroom', primaryjoin='TemperatureHumidityDatum.ClassroomID == Classroom.ClassroomID', backref='temperature_humidity_data')
--- server.py
+++ server.py
@@ -4,7 +4,16 @@ from flask import render_template
 @app.route("/")
 def home():
     print(LoudnessData.query.with_entities(LoudnessData.LoudnessReading, LoudnessData.ClassroomID).all())
-    return(render_template("main.html"))
+    return(render_template("pages/main.html"))
 
+
+@app.route("/classroom/<string:classroom_id>")
+def analytics(classroom_id):
+    print(classroom_id)
+    return(render_template("pages/analytics.html", classroom = classroom_id))
+
+@app.route("/about")
+def about():
+    return(render_template("pages/about.html"))
 if "__main__" == __name__:
     app.run(debug=True) 
