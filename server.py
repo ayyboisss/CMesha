63dfd37ce251711229aefb1820b43c81c74260cb
ayyboisss <kentquilestino.lols@gmail.com>
2024-05-23 13:41:13 +1200

    Output testing

--- server.py
+++ server.py
@@ -12,7 +12,10 @@ def home():
 @app.route("/classroom/<string:classroom_id>")
 def analytics(classroom_id):
     print(classroom_id)
-    return(render_template("pages/analytics.html", classroom = classroom_id))
+    loudness = LoudnessData.query.filter_by(
+        ClassroomID = classroom_id).with_entities(
+        LoudnessData.ClassroomID, LoudnessData.LoudnessReading).all()
+    return(render_template("pages/analytics.html", classroom = classroom_id, loudness = loudness))
 
 @app.route("/about")
 def about():
