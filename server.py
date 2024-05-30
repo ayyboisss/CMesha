23600b3006ab81bb008cd1561304a2d2a0a48eac
BURNSIDE HIGH SCHOOL <code@burnside.school.nz>
2024-05-30 14:11:33 +1200

    finish basic nav

--- server.py
+++ server.py
@@ -6,24 +6,24 @@ from flask import render_template
 def home():
     classrooms = Classroom.query.with_entities(
                     Classroom.ClassroomID).all()
-    return(render_template("pages/main.html", classrooms = classrooms))
+    return (render_template("pages/main.html", classrooms=classrooms))
 
 
 @app.route("/classroom/<string:classroom_id>")
 def analytics(classroom_id):
     print(classroom_id)
     loudness = LoudnessData.query.filter_by(
-        ClassroomID = classroom_id).with_entities(
+        ClassroomID=classroom_id).with_entities(
         LoudnessData.ClassroomID, LoudnessData.LoudnessReading).all()
-    return(render_template("pages/analytics.html", classroom = classroom_id, loudness = loudness))
+    return (render_template("pages/analytics.html", classroom=classroom_id, loudness=loudness))
 
 @app.route("/staff")
 def staff():
-    return(render_template("pages/staff.html"))
+    return (render_template("pages/staff.html"))
 
 @app.route("/about")
 def about():
-    return(render_template("pages/about.html"))
+    return (render_template("pages/about.html"))
 
 
 if "__main__" == __name__:
