f4df8c6c635b91a8b67079caa614fd104c0281fd
BURNSIDE HIGH SCHOOL <code@burnside.school.nz>
2024-05-30 14:15:41 +1200

    Added more queries for the server

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from database import app, Classroom, LoudnessData
+from database import app, Classroom, LoudnessData, TemperatureHumidityDatum, AnemometerDatum
 from flask import render_template
 
 
@@ -11,10 +11,22 @@ def home():
 
 @app.route("/classroom/<string:classroom_id>")
 def analytics(classroom_id):
-    print(classroom_id)
+    # Queries
     loudness = LoudnessData.query.filter_by(
         ClassroomID=classroom_id).with_entities(
         LoudnessData.ClassroomID, LoudnessData.LoudnessReading).all()
+    
+    temperature = TemperatureHumidityDatum.query.filter_by(
+        ClassroomID=classroom_id).with_entities(
+        TemperatureHumidityDatum.TemperatureReading).all()
+    
+    humidity = TemperatureHumidityDatum.query.filter_by(
+        ClassroomID=classroom_id).with_entities(
+        TemperatureHumidityDatum.HumidityReading).all()
+    
+    wind_speed
+    
+
     return (render_template("pages/analytics.html", classroom=classroom_id, loudness=loudness))
 
 @app.route("/staff")
@@ -27,4 +39,4 @@ def about():
 
 
 if "__main__" == __name__:
-    app.run(debug=True) 
+    app.run(debug=True)
