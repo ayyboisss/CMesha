e993cd51651ea42ee0086748939f24cb11c8e303
BURNSIDE HIGH SCHOOL <code@burnside.school.nz>
2024-06-04 12:39:37 +1200

    Use fr instead of %

--- server.py
+++ server.py
@@ -15,28 +15,29 @@ def analytics(classroom_id):
     loudness = LoudnessData.query.filter_by(
         ClassroomID=classroom_id).with_entities(
         LoudnessData.LoudnessReading).all()
-    
+
     temperature = TemperatureHumidityDatum.query.filter_by(
         ClassroomID=classroom_id).with_entities(
         TemperatureHumidityDatum.TemperatureReading).all()
-    
+
     humidity = TemperatureHumidityDatum.query.filter_by(
         ClassroomID=classroom_id).with_entities(
         TemperatureHumidityDatum.HumidityReading).all()
-    
+
     wind_speed = AnemometerDatum.query.filter_by(
         ClassroomID=classroom_id).with_entities(
         AnemometerDatum.AnemometerReading).all()
-    
 
     return (render_template("pages/analytics.html",
-                             classroom=classroom_id, loudness=loudness,
-                             temperature=temperature, humidity=humidity,
-                             wind_speed=wind_speed))
+                            classroom=classroom_id, loudness=loudness,
+                            temperature=temperature, humidity=humidity,
+                            wind_speed=wind_speed))
+
 
 @app.route("/staff")
 def staff():
     return (render_template("pages/staff.html"))
+
 
 @app.route("/about")
 def about():
