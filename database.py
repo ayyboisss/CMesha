ffe378dbd9dce7df44cc0bb57e956eb2c393c9c3
BURNSIDE HIGH SCHOOL <code@burnside.school.nz>
2024-06-05 14:15:54 +1200

    small fixes related to pep8

--- database.py
+++ database.py
@@ -27,14 +27,12 @@ class AnemometerDatum(db.Model):
     Classroom = db.relationship('Classroom', primaryjoin='AnemometerDatum.ClassroomID == Classroom.ClassroomID', backref='anemometer_data')
 
 
-
 class Classroom(db.Model):
     __tablename__ = 'Classrooms'
 
     ClassroomID = db.Column(db.Text, primary_key=True)
 
     Teachers = db.relationship('Teacher', secondary='TeacherClassrooms', backref='classrooms')
-
 
 
 class LoudnessData(db.Model):
@@ -48,13 +46,11 @@ class LoudnessData(db.Model):
     Classroom = db.relationship('Classroom', primaryjoin='LoudnessData.ClassroomID == Classroom.ClassroomID', backref='loudness_data')
 
 
-
 t_TeacherClassrooms = db.Table(
     'TeacherClassrooms',
     db.Column('TeacherID', db.ForeignKey('Teachers.ID')),
     db.Column('ClassroomID', db.ForeignKey('Classrooms.ClassroomID'))
 )
-
 
 
 class Teacher(db.Model):
@@ -63,7 +59,6 @@ class Teacher(db.Model):
     ID = db.Column(db.Integer, primary_key=True)
     TeacherName = db.Column(db.Text)
     TeacherCode = db.Column(db.Text)
-
 
 
 class TemperatureHumidityDatum(db.Model):
