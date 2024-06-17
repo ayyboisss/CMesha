9a93e15d308df04d9ed77959c60e11302555abdf
BURNSIDE HIGH SCHOOL <code@burnside.school.nz>
2024-06-17 12:40:58 +1200

    Fix this

--- server.py
+++ server.py
@@ -2,31 +2,76 @@ from database import app, Classroom, LoudnessData, TemperatureHumidityDatum, Ane
 from flask import render_template
 
 
+def kowalski_analyze(classroom_id=str):
+    "Get all data relevent to the specific classroom"
+    result = []
+    result.append(classroom_id)
+
+    # Loudness
+    result.append(LoudnessData.query.filter_by(
+        ClassroomID=classroom_id).with_entities(
+        LoudnessData.LoudnessReading).all())
+
+    # Temperature / Humidity
+    result.append(TemperatureHumidityDatum.query.filter_by(
+        ClassroomID=classroom_id).with_entities(
+        TemperatureHumidityDatum.TemperatureReading).all())
+
+    result.append(TemperatureHumidityDatum.query.filter_by(
+        ClassroomID=classroom_id).with_entities(
+        TemperatureHumidityDatum.HumidityReading).all())
+
+    # Wind speed
+    result.append(AnemometerDatum.query.filter_by(
+        ClassroomID=classroom_id).with_entities(
+        AnemometerDatum.AnemometerReading).all())
+
+    return result
+
+
 @app.route("/")
 def home():
     classrooms = Classroom.query.with_entities(
                     Classroom.ClassroomID).limit(6).all()
-    return (render_template("pages/main.html", classrooms=classrooms))
+
+    giga_list = []
+    for i in classrooms:
+        print(i[0])
+        # Classroom ID, this is technically creating a table in python
+        giga_list.append(i)
+
+        # Yep, we're doing this again
+        loudness = LoudnessData.query.filter_by(
+            ClassroomID=i[0]).with_entities(
+            LoudnessData.LoudnessReading).first()
+        giga_list.append(loudness)
+
+        temperature = TemperatureHumidityDatum.query.filter_by(
+            ClassroomID=i[0]).with_entities(
+            TemperatureHumidityDatum.TemperatureReading).first()
+        giga_list.append(temperature)
+
+        humidity = TemperatureHumidityDatum.query.filter_by(
+            ClassroomID=i[0]).with_entities(
+            TemperatureHumidityDatum.HumidityReading).first()
+        giga_list.append(humidity)
+
+        wind_speed = AnemometerDatum.query.filter_by(
+            ClassroomID=i[0]).with_entities(
+            AnemometerDatum.AnemometerReading).first()
+        giga_list.append(wind_speed)
+
+    print(giga_list)
+    return (render_template("pages/main.html", giga_list=giga_list))
 
 
 @app.route("/classroom/<string:classroom_id>")
 def analytics(classroom_id):
-    # Queries
-    loudness = LoudnessData.query.filter_by(
-        ClassroomID=classroom_id).with_entities(
-        LoudnessData.LoudnessReading).all()
-
-    temperature = TemperatureHumidityDatum.query.filter_by(
-        ClassroomID=classroom_id).with_entities(
-        TemperatureHumidityDatum.TemperatureReading).all()
-
-    humidity = TemperatureHumidityDatum.query.filter_by(
-        ClassroomID=classroom_id).with_entities(
-        TemperatureHumidityDatum.HumidityReading).all()
-
-    wind_speed = AnemometerDatum.query.filter_by(
-        ClassroomID=classroom_id).with_entities(
-        AnemometerDatum.AnemometerReading).all()
+    data = kowalski_analyze(classroom_id)
+    loudness = data[1]
+    temperature = data[2]
+    humidity = data[3]
+    wind_speed = data[4]
 
     return (render_template("pages/analytics.html",
                             classroom=classroom_id, loudness=loudness,
